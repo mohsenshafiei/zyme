3bd701544820323cad3a75a37c19f25e04bb2d77
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2024-09-04 17:52:30 -0400

    chore(root): add clean script

---
+++ scripts/clean.js
@@ -0,0 +1,20 @@
+import { rimraf } from 'rimraf';
+import ora from 'ora';
+import pc from 'picocolors';
+
+const removeDirectory = async (path, options) => {
+  const spinner = ora(`Removing ${path}...`).start();
+  try {
+    const result = await rimraf(path, options);
+    if (result instanceof Error) {
+      spinner.fail(pc.red(`Error removing ${path}: ${result.message}`));
+    } else {
+      spinner.succeed(pc.green(`${path} folder removed.`));
+    }
+  } catch (error) {
+    spinner.fail(pc.red(`Unexpected error removing ${path}: ${error}`));
+  }
+};
+
+removeDirectory('node_modules');
+removeDirectory('dist');
